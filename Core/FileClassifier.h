5be6712529e0f1efc7661b1da10a94f4b6de3cce
ZonD <vip.leontev2002@gmail.com>
2026-06-29 20:18:11 +0900

    Добавление всех форматов, но нужно доделать, есть косяки

--- Core/FileClassifier.h
+++ Core/FileClassifier.h
@@ -7,6 +7,7 @@ enum class FileType {
     Video,
     Audio,
     Image,
+    Raw,
     Unknown
 };
 
@@ -22,12 +23,16 @@ public:
             L".mp3", L".wav", L".flac", L".ogg", L".m4a", L".aac", L".wma", L".opus"
         };
         static const std::vector<std::wstring> imageExts = {
-            L".jpg", L".jpeg", L".png", L".bmp", L".webp", L".tiff", L".gif"
+            L".jpg", L".jpeg", L".png", L".bmp", L".webp", L".tiff", L".gif", L".heif", L".heic"
+        };
+        static const std::vector<std::wstring> rawExts = {
+            L".cr2", L".cr3", L".nef", L".arw", L".dng", L".raf", L".orf", L".rw2", L".pef", L".srw", L".raw"
         };
 
         if (contains(videoExts, ext)) return FileType::Video;
         if (contains(audioExts, ext)) return FileType::Audio;
         if (contains(imageExts, ext)) return FileType::Image;
+        if (contains(rawExts, ext))   return FileType::Raw;
         return FileType::Unknown;
     }
 
@@ -36,6 +41,7 @@ public:
         case FileType::Video: return "Video";
         case FileType::Audio: return "Audio";
         case FileType::Image: return "Image";
+        case FileType::Raw: return "Raw";
         default: return "Unknown";
         }
     }
